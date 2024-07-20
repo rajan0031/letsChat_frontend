2ae6301c1214f4f6eb8ecffc4fa815334a0166de
rajan0031 <109027109+rajan0031@users.noreply.github.com>
2024-07-20 21:26:21 +0530

    this is the adding the file

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5000";
+// export const host = "http://localhost:5000";
+export const host = "https://letschat-backend-6iza.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
